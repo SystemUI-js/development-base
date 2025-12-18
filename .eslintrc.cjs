06e5ec3a9bdb15a6b61bed470cf28d2ba09418c0
ZX <wszxdhr@126.com>
2025-12-18 20:19:35 +0800

    feat: 增加 react 专用 eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,29 +11,18 @@ module.exports = {
     sourceType: "module",
     ecmaFeatures: { jsx: true }
   },
-  settings: {
-    react: { version: "detect" }
-  },
   plugins: [
     "@typescript-eslint",
-    "react",
-    "react-hooks",
     "jsx-a11y",
     "prettier"
   ],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and displays Prettier issues as ESLint errors
   ],
   rules: {
-    // React 17+ JSX transform
-    "react/react-in-jsx-scope": "off",
-    // We use TypeScript for types
-    "react/prop-types": "off",
     // Prefer TS-aware unused vars rule
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": [
